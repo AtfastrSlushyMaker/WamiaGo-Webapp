5a7ebb8893f07237e10dd8a49bbd13ff21ff2c2b
Abd Razek Nakhli <abdrazek.nakhli@esprit.tn>
2025-04-03 14:03:27 +0000

    user enum

---
+++ src/Enum/DRIVER_ROLE.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Enum;
+
+enum DRIVER_ROLE: string
+{
+    case TAXI_DRIVER = 'TAXI_DRIVER';
+    case TRANSPORTER = 'TRANSPORTER';
+    case CARPOOL_DRIVER = 'CARPOOL_DRIVER';
+}
