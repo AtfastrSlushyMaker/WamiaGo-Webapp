1b782de6a7ebacab122df71ecb811440b3c7a9db
Abd Razek Nakhli <abdrazek.nakhli@esprit.tn>
2025-04-05 22:57:03 +0100

    back office dark mode and navigation done

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -11,7 +11,6 @@ class AdminController extends AbstractController
     #[Route('/admin', name: 'admin_dashboard')]
     public function dashboard(): Response
     {
-
         $stats = [
             'rideShares' => 150,
             'taxiBookings' => 53,
@@ -22,5 +21,47 @@ class AdminController extends AbstractController
         return $this->render('back-office/dashboard.html.twig', [
             'stats' => $stats
         ]);
+    }
+
+    #[Route('/admin/users', name: 'admin_users')]
+    public function users(): Response
+    {
+        return $this->render('back-office/users.html.twig');
+    }
+
+    #[Route('/admin/ride-sharing', name: 'admin_ride_sharing')]
+    public function rideSharing(): Response
+    {
+        return $this->render('back-office/ride-sharing.html.twig');
+    }
+
+    #[Route('/admin/taxi-bookings', name: 'admin_taxi_bookings')]
+    public function taxiBookings(): Response
+    {
+        return $this->render('back-office/taxi-bookings.html.twig');
+    }
+
+    #[Route('/admin/bicycle-rentals', name: 'admin_bicycle_rentals')]
+    public function bicycleRentals(): Response
+    {
+        return $this->render('back-office/bicycle-rentals.html.twig');
+    }
+
+    #[Route('/admin/relocations', name: 'admin_relocations')]
+    public function relocations(): Response
+    {
+        return $this->render('back-office/relocations.html.twig');
+    }
+
+    #[Route('/admin/settings', name: 'admin_settings')]
+    public function settings(): Response
+    {
+        return $this->render('back-office/settings.html.twig');
+    }
+
+    #[Route('/admin/profile', name: 'admin_profile')]
+    public function profile(): Response
+    {
+        return $this->render('back-office/profile.html.twig');
     }
 }
