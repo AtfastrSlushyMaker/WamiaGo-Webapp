2ece413fd5f339212a060af4cac440df817b46ce
Abd Razek Nakhli <abdrazek.nakhli@esprit.tn>
2025-04-05 17:20:25 +0100

    dashboard gg

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -11,6 +11,16 @@ class AdminController extends AbstractController
     #[Route('/admin', name: 'admin_dashboard')]
     public function dashboard(): Response
     {
-        return $this->render('back-office/dashboard.html.twig');
+
+        $stats = [
+            'rideShares' => 150,
+            'taxiBookings' => 53,
+            'bicycleRentals' => 44,
+            'relocationBookings' => 65,
+        ];
+
+        return $this->render('back-office/dashboard.html.twig', [
+            'stats' => $stats
+        ]);
     }
 }
